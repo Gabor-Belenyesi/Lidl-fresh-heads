097694c136e9e6bc5f8259f42f951ec6e87750e5
Gabor-Belenyesi <belenyesig@gmail.com>
2022-10-08 20:56:05 +0200

    ready

---
+++ script.js
@@ -0,0 +1,281 @@
+const avo = document.querySelector(".avo");
+const div1 = document.querySelector(".div1");
+
+avo.addEventListener("click", function(){
+    const qr1 = document.querySelector(".qr1");
+    qr1.classList.add("qr1-c")
+   
+});
+const qr1 = document.querySelector(".qr1");
+qr1.addEventListener("click", function(){
+    qr1.classList.remove("qr1-c");
+});
+
+const caro = document.querySelector(".carro")
+caro.addEventListener("click", function(){
+    const qr2 = document.querySelector(".qr2");
+    qr2.classList.add("qr1-c")
+   
+});
+const qr2 = document.querySelector(".qr2");
+qr2.addEventListener("click", function(){
+    qr2.classList.remove("qr1-c");
+});
+const castro = document.querySelector(".castro")
+castro.addEventListener("click", function(){
+    const qr3 = document.querySelector(".qr3");
+    qr3.classList.add("qr1-c")
+   
+});
+const qr3 = document.querySelector(".qr3");
+qr3.addEventListener("click", function(){
+    qr3.classList.remove("qr1-c");
+});
+
+const cici = document.querySelector(".cici")
+cici.addEventListener("click", function(){
+    const qr4 = document.querySelector(".qr4");
+    qr4.classList.add("qr1-c")
+   
+});
+const qr4 = document.querySelector(".qr4");
+qr4.addEventListener("click", function(){
+    qr4.classList.remove("qr1-c");
+});
+
+const cipi = document.querySelector(".cipi")
+cipi.addEventListener("click", function(){
+    const qr5 = document.querySelector(".qr5");
+    qr5.classList.add("qr1-c")
+   
+});
+const qr5 = document.querySelector(".qr5");
+qr5.addEventListener("click", function(){
+    qr5.classList.remove("qr1-c");
+});
+
+const drago = document.querySelector(".drago")
+drago.addEventListener("click", function(){
+    const qr6 = document.querySelector(".qr6");
+    qr6.classList.add("qr1-c")
+   
+});
+const qr6 = document.querySelector(".qr6");
+qr6.addEventListener("click", function(){
+    qr6.classList.remove("qr1-c");
+});
+
+const fifi = document.querySelector(".fifi")
+fifi.addEventListener("click", function(){
+    const qr7 = document.querySelector(".qr7");
+    qr7.classList.add("qr1-c")
+   
+});
+const qr7 = document.querySelector(".qr7");
+qr7.addEventListener("click", function(){
+    qr7.classList.remove("qr1-c");
+});
+
+const hallo = document.querySelector(".hallo")
+hallo.addEventListener("click", function(){
+    const qr8 = document.querySelector(".qr8");
+    qr8.classList.add("qr1-c")
+   
+});
+const qr8 = document.querySelector(".qr8");
+qr8.addEventListener("click", function(){
+    qr8.classList.remove("qr1-c");
+});
+
+const kiki = document.querySelector(".kiki")
+kiki.addEventListener("click", function(){
+    const qr9 = document.querySelector(".qr9");
+    qr9.classList.add("qr1-c")
+   
+});
+const qr9 = document.querySelector(".qr9");
+qr9.addEventListener("click", function(){
+    qr9.classList.remove("qr1-c");
+});
+
+const leti = document.querySelector(".leti")
+leti.addEventListener("click", function(){
+    const qr10 = document.querySelector(".qr10");
+    qr10.classList.add("qr1-c")
+   
+});
+const qr10 = document.querySelector(".qr10");
+qr10.addEventListener("click", function(){
+    qr10.classList.remove("qr1-c");
+});
+
+const lila = document.querySelector(".lila")
+lila.addEventListener("click", function(){
+    const qr11 = document.querySelector(".qr11");
+    qr11.classList.add("qr1-c")
+   
+});
+const qr11 = document.querySelector(".qr11");
+qr11.addEventListener("click", function(){
+    qr11.classList.remove("qr1-c");
+});
+
+const latzu = document.querySelector(".latzu")
+latzu.addEventListener("click", function(){
+    const qr12 = document.querySelector(".qr12");
+    qr12.classList.add("qr1-c")
+   
+});
+const qr12 = document.querySelector(".qr12");
+qr12.addEventListener("click", function(){
+    qr12.classList.remove("qr1-c");
+});
+
+const nana = document.querySelector(".nana")
+nana.addEventListener("click", function(){
+    const qr13 = document.querySelector(".qr13");
+    qr13.classList.add("qr1-c")
+   
+});
+const qr13 = document.querySelector(".qr13");
+qr13.addEventListener("click", function(){
+    qr13.classList.remove("qr1-c");
+});
+
+const pepita = document.querySelector(".pepita")
+pepita.addEventListener("click", function(){
+    const qr14 = document.querySelector(".qr14");
+    qr14.classList.add("qr1-c")
+   
+});
+const qr14 = document.querySelector(".qr14");
+qr14.addEventListener("click", function(){
+    qr14.classList.remove("qr1-c");
+});
+
+const porto = document.querySelector(".porto")
+porto.addEventListener("click", function(){
+    const qr15 = document.querySelector(".qr15");
+    qr15.classList.add("qr1-c")
+   
+});
+const qr15 = document.querySelector(".qr15");
+qr15.addEventListener("click", function(){
+    qr15.classList.remove("qr1-c");
+});
+
+const pufi = document.querySelector(".pufi")
+pufi.addEventListener("click", function(){
+    const qr16 = document.querySelector(".qr16");
+    qr16.classList.add("qr1-c")
+   
+});
+const qr16 = document.querySelector(".qr16");
+qr16.addEventListener("click", function(){
+    qr16.classList.remove("qr1-c");
+});
+
+const riki = document.querySelector(".riki")
+riki.addEventListener("click", function(){
+    const qr17 = document.querySelector(".qr17");
+    qr17.classList.add("qr1-c")
+   
+});
+const qr17 = document.querySelector(".qr17");
+qr17.addEventListener("click", function(){
+    qr17.classList.remove("qr1-c");
+});
+
+const rodi = document.querySelector(".rodi")
+rodi.addEventListener("click", function(){
+    const qr18 = document.querySelector(".qr18");
+    qr18.classList.add("qr1-c")
+   
+});
+const qr18 = document.querySelector(".qr18");
+qr18.addEventListener("click", function(){
+    qr18.classList.remove("qr1-c");
+});
+
+const rombi = document.querySelector(".rombi")
+rombi.addEventListener("click", function(){
+    const qr19 = document.querySelector(".qr19");
+    qr19.classList.add("qr1-c")
+   
+});
+const qr19 = document.querySelector(".qr19");
+qr19.addEventListener("click", function(){
+    qr19.classList.remove("qr1-c");
+});
+
+const spooks = document.querySelector(".spooks")
+spooks.addEventListener("click", function(){
+    const qr20 = document.querySelector(".qr20");
+    qr20.classList.add("qr1-c")
+   
+});
+const qr20 = document.querySelector(".qr20");
+qr20.addEventListener("click", function(){
+    qr20.classList.remove("qr1-c");
+});
+
+const tango = document.querySelector(".tango")
+tango.addEventListener("click", function(){
+    const qr21 = document.querySelector(".qr21");
+    qr21.classList.add("qr1-c")
+   
+});
+const qr21 = document.querySelector(".qr21");
+qr21.addEventListener("click", function(){
+    qr21.classList.remove("qr1-c");
+});
+
+const tomescu = document.querySelector(".tomescu")
+tomescu.addEventListener("click", function(){
+    const qr22 = document.querySelector(".qr22");
+    qr22.classList.add("qr1-c")
+   
+});
+const qr22 = document.querySelector(".qr22");
+qr22.addEventListener("click", function(){
+    qr22.classList.remove("qr1-c");
+});
+
+const una = document.querySelector(".una")
+una.addEventListener("click", function(){
+    const qr23 = document.querySelector(".qr23");
+    qr23.classList.add("qr1-c")
+   
+});
+const qr23 = document.querySelector(".qr23");
+qr23.addEventListener("click", function(){
+    qr23.classList.remove("qr1-c");
+});
+
+const yona = document.querySelector(".yona")
+yona.addEventListener("click", function(){
+    const qr24 = document.querySelector(".qr24");
+    qr24.classList.add("qr1-c")
+   
+});
+const qr24 = document.querySelector(".qr24");
+qr24.addEventListener("click", function(){
+    qr24.classList.remove("qr1-c");
+});
+
+const zuzi = document.querySelector(".zuzi")
+zuzi.addEventListener("click", function(){
+    const qr25 = document.querySelector(".qr25");
+    qr25.classList.add("qr1-c")
+   
+});
+const qr25 = document.querySelector(".qr25");
+qr25.addEventListener("click", function(){
+    qr25.classList.remove("qr1-c");
+});
+
+
+
+
+
+
